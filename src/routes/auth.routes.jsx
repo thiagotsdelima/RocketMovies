1f8a1f20a2d8dce660cc4198c55f9f952ade3af8
thiago <thiagotsdelima@gmail.com>
2024-01-05 14:37:43 -0300

    up

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom"
 
-import  SignIn  from "../pages/SignIn"
-import  SignUp  from "../pages/SignUp"
+import { SignIn } from "../pages/SignIn"
+import { SignUp } from "../pages/SignUp"
 
-
+console.log('/src/pages/SignIn:', "../pages/SignIn");
 
 export function AuthRoutes() {
 
