4bab4f738a78ad1086c94f2bf0a05e3458381149
thiago <thiagotsdelima@gmail.com>
2024-01-05 13:46:06 -0300

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-import theme from './Styles/theme'
-import GlobalStyles from './Styles/global'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
 import { AuthProvider } from './Hooks/auth'
 import { Routes } from './Routes'
 
