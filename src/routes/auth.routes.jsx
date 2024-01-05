f11ab9439ae2c71edad6c55688f7e8b0c386d800
thiago <thiagotsdelima@gmail.com>
2024-01-05 15:04:48 -0300

    created index.html

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 
-import { SignIn } from "../pages/signIn"
+import { SignIn } from "../pages/SignIn"
 import { SignUp } from "../pages/SignUp"
 
 export function AuthRoutes() {
